68c8129440f3db1a467aa12fd7af6e5e146c7f21
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2018-02-12 20:10:06 +0300

    do not use functions not available in kotlin-runtime, better handle size overflow on resizing

--- core/runtime.jvm/src/kotlin/jvm/internal/CollectionToArray.kt
+++ core/runtime.jvm/src/kotlin/jvm/internal/CollectionToArray.kt
@@ -3,42 +3,42 @@
  * that can be found in the license/LICENSE.txt file.
  */
 
-/*
- * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
- * that can be found in the license/LICENSE.txt file.
- */
-
-@file:kotlin.jvm.JvmName("CollectionToArray")
+@file:JvmName("CollectionToArray")
 
 package kotlin.jvm.internal
 
-fun toArray(collection: Collection<*>): Array<Any?> =
+import java.util.Arrays
+
+
+@JvmName("toArray")
+fun collectionToArray(collection: Collection<*>): Array<Any?> =
     toArrayImpl(
         collection,
         empty = { emptyArray() },
         alloc = { size -> arrayOfNulls<Any?>(size) },
-        trim = { result, size -> result.copyOf(size) }
+        trim = { result, size -> Arrays.copyOf(result, size) }
     )
 
 // Note: Array<Any?> here can have any reference array JVM type at run time
-fun toArray(collection: Collection<*>, a: Array<Any?>?): Array<Any?> {
+@JvmName("toArray")
+fun collectionToArray(collection: Collection<*>, a: Array<Any?>?): Array<Any?> {
     // Collection.toArray contract requires that NullPointerException is thrown when array is null
-    if (a == null) throw NullPointerException()
+    if (a == null) throw java.lang.NullPointerException()
     return toArrayImpl(
         collection,
         empty = {
-            if (a.isNotEmpty()) a[0] = null
+            if (a.size > 0) a[0] = null
             a
         },
         alloc = { size ->
-            if (size <= a.size) a else a.copyOf(size)
+            if (size <= a.size) a else java.lang.reflect.Array.newInstance(a.javaClass.componentType, size) as Array<Any?>
         },
         trim = { result, size ->
             if (result === a) {
                 a[size] = null
                 a
             } else
-                result.copyOf(size)
+                Arrays.copyOf(result, size)
         }
     )
 }
@@ -61,7 +61,13 @@ private inline fun toArrayImpl(
         if (i >= result.size) {
             if (!iter.hasNext()) return result // perfect match of array size
             // array size was too small -- grow array
-            result = result.copyOf(i * 2)
+            var newSize = i * 2
+            if (newSize < 0) {
+                if (i == Int.MAX_VALUE) throw OutOfMemoryError()
+                newSize = Int.MAX_VALUE
+            }
+            result = Arrays.copyOf(result, newSize)
+
         } else {
             if (!iter.hasNext()) return trim(result, i) // ended too early (allocated array too big)
         }
