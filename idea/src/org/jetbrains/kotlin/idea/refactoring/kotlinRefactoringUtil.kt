d9f0cf405ccd8afaaecc33d032d1aa7259299149
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2018-02-16 20:51:05 +0300

    Move: Drop ${...} around shortened qualified expression
     #KT-22692 Fixed

--- idea/src/org/jetbrains/kotlin/idea/refactoring/kotlinRefactoringUtil.kt
+++ idea/src/org/jetbrains/kotlin/idea/refactoring/kotlinRefactoringUtil.kt
@@ -62,6 +62,7 @@ import org.jetbrains.kotlin.idea.KotlinLanguage
 import org.jetbrains.kotlin.idea.caches.resolve.*
 import org.jetbrains.kotlin.idea.codeInsight.DescriptorToSourceUtilsIde
 import org.jetbrains.kotlin.idea.core.ShortenReferences
+import org.jetbrains.kotlin.idea.core.dropBraces
 import org.jetbrains.kotlin.idea.core.getPackage
 import org.jetbrains.kotlin.idea.core.quoteSegmentsIfNeeded
 import org.jetbrains.kotlin.idea.core.util.showYesNoCancelDialog
@@ -783,7 +784,7 @@ fun KtExpression.removeTemplateEntryBracesIfPossible(): KtExpression {
     if (parent !is KtBlockStringTemplateEntry) return this
 
     val intention = RemoveCurlyBracesFromTemplateIntention()
-    val newEntry = if (intention.isApplicableTo(parent)) intention.applyTo(parent) else parent
+    val newEntry = if (intention.isApplicableTo(parent)) parent.dropBraces() else parent
     return newEntry.expression!!
 }
 
